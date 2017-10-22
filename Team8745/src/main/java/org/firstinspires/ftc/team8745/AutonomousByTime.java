6f8e4a0017c9ebbf72ba45697770533755f4c106
rose <crashtest64@micahsolomon.com>
2017-10-22 16:41:54 -0700

    First Autonomous! Yay!

--- Team8745/src/main/java/org/firstinspires/ftc/team8745/AutonomousByTime.java
+++ Team8745/src/main/java/org/firstinspires/ftc/team8745/AutonomousByTime.java
@@ -1,8 +1,65 @@
 package org.firstinspires.ftc.team8745;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
 /**
  * Created by rose on 10/22/17.
  */
 
-public class AutonomousByTime {
+
+@Autonomous(name="Pushbot: Auto Drive By Time", group="Pushbot")
+
+public class AutonomousByTime extends LinearOpMode {
+
+
+    /* Declare OpMode members. */
+    OmniDriveRobot robot = new OmniDriveRobot();
+    private ElapsedTime elapsedTime = new ElapsedTime();
+
+
+    static final double     FORWARD_SPEED = 0.6;
+    static final double     REVERSE_SPEED = -0.6;
+    static final double     TURN_SPEED    = 0.5;
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
+
+        // Step 1:  Drive forward for 3 seconds
+        robot.A.setPower(FORWARD_SPEED);
+        robot.D.setPower(FORWARD_SPEED);
+
+        robot.B.setPower(REVERSE_SPEED);
+        robot.C.setPower(REVERSE_SPEED);
+
+        elapsedTime.reset();
+        while (opModeIsActive() && (elapsedTime.seconds() < 3.0)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", elapsedTime.seconds());
+            telemetry.update();
+        }
+
+        robot.A.setPower(0);
+        robot.B.setPower(0);
+        robot.C.setPower(0);
+        robot.D.setPower(0);
+    }
 }
