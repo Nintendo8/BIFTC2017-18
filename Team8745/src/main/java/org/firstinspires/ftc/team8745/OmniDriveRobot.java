7e1367f099d61368d69d5a77d4d4f3ac8966b2fd
rose <crashtest64@micahsolomon.com>
2017-11-26 17:01:29 -0800

    IMU stuff! Yay

--- Team8745/src/main/java/org/firstinspires/ftc/team8745/OmniDriveRobot.java
+++ Team8745/src/main/java/org/firstinspires/ftc/team8745/OmniDriveRobot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.team8745;
 
+        import com.qualcomm.hardware.bosch.BNO055IMU;
+        import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
         import com.qualcomm.robotcore.hardware.ColorSensor;
         import com.qualcomm.robotcore.hardware.DcMotor;
         import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -36,6 +38,8 @@ public class OmniDriveRobot {
 
     public ColorSensor jewelSensor;
 
+    public BNO055IMU imu;
+
     public void init(HardwareMap hardwareMap) {
 
         /*lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -63,6 +67,23 @@ public class OmniDriveRobot {
         jewelSensor = hardwareMap.colorSensor.get("Jewel Sensor");
 
         jewelServo.setPosition(1.0);
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
     }
 
---
+++ Team8745/src/main/java/org/firstinspires/ftc/team8745/telemetryIMU.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.team8745;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+/**
+ * Created by rose on 11/26/17.
+ */
+
+@Autonomous(name ="IMU Telemetry")
+public class telemetryIMU extends OpMode {
+
+    OmniDriveRobot robot = new OmniDriveRobot();
+
+    @Override
+    public void init(){
+    robot.init(hardwareMap);
+
+    }
+
+    @Override
+    public void loop(){
+    Orientation angles   = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    telemetry.addData("Angles", angles.firstAngle);
+
+    }
+
+}
